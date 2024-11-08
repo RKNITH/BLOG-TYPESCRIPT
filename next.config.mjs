c2b8daea1c30d6a00e14180d7b03324c7478b294
RKNITH <raviranjankofficial@gmail.com>
2024-11-08 15:35:20 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        remotePatterns: [
+            {
+                hostname:"shahriarblog.oneentry.cloud"
+            }
+        ]
+    }
+};
+
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/shared/modules/home";
+import Articles from "@/shared/modules/articles/articles";
+
+const Page = () => {
+  return (
+    <div>
+      <Hero />
+      <Articles />
+    </div>
+  );
+};
+
+export default Page;
